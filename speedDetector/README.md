5104ecdd83d20ded3f7a21e6de93b089a923c1e8
+ <+>
2023-03-20 00:34:00 +0300

    Speed Detector test

---
+++ speedDetector/README.md
@@ -0,0 +1,11 @@
+ This is a speed detector system
+I am required to come up the the solution to the problem below
+
+/*Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.*/
+
+
+I will use :
+=>HTML for my GUI
+=>CSS for styling
+=> Javascript for my backend
+
---
+++ speedDetector/style.js
@@ -0,0 +1,38 @@
+body {
+    background-color: darkpurple;
+    color: white;
+}
+
+h1 {
+    text-align: center;
+}
+
+label {
+    display: block;
+    margin: 20px 0;
+}
+
+input[type=number] {
+    padding: 10px;
+    font-size: 16px;
+    border: none;
+    border-radius: 5px;
+    margin-right: 10px;
+}
+
+button {
+    background-color: white;
+    color: darkpurple;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+#result {
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 20px;
+}
